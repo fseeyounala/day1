c569907a84e4bcda98776954818f371150e32ddc
fuu <1440233578@qq.com>
2022-06-19 09:07:23 +0800

    第三次提交

--- jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
+++ jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
@@ -3,6 +3,8 @@ package tedu.com.jtspringboot.controler;
 public class JbController {
     public static void main(String[] args) {
         System.out.println("223232");
+        System.out.println("第三次提交");
+        System.out.println();
     }
 
 }
