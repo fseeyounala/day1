e4814e98162368477c1eeb06f4ded76fea44adbd
fuu <1440233578@qq.com>
2022-06-19 09:50:27 +0800

    分支添加

--- jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
+++ jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
@@ -2,7 +2,19 @@ package tedu.com.jtspringboot.controler;
 
 public class JbController {
     public static void main(String[] args) {
-        System.out.println("223232");
+
+        System.out.println("223232");        System.out.println("223232");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+        System.out.println("分支添加");
+
+        System.out.println("分支添加");
+
     }
 
 }
