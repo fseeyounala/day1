403c09e9a10fa64297833c42d5a819d6da413f6c
fuu <1440233578@qq.com>
2022-06-19 15:23:32 +0800

    克隆者添加

--- jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
+++ jt-springboot/src/main/java/tedu/com/jtspringboot/controler/JbController.java
@@ -5,21 +5,7 @@ public class JbController {
         System.out.println("223232");
         System.out.println("第三次提交");
         System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("主干添加");
-        System.out.println("最后一次插入数据");
-System.out.println("zai git上最后一次插入数据");
-        System.out.println("223232");        System.out.println("223232");
-        System.out.println("分支添加");
-        System.out.println("验证在git上修改数据");
-        System.out.println("拉去数据");
-        System.out.println("拉去数据");
-        System.out.println("拉去数据");
-        System.out.println("拉去数据");
+        System.out.println("克隆这添加");
 
 
     }
